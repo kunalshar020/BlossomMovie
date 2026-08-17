206d91e13c61c2ada6c96cb8ba36686c14859a44
indiedev02 <dev.kunalshar0@gmail.com>
2026-08-17 11:48:18 +0530

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Kunal Sharma on 17/08/26.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
